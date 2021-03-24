350acfea3906d1233409d139e7aed2054c3d4935
Tomasz Nowak <mychaty@gmail.com>
2021-03-25 00:04:10 +0100

    Create basic parameters grid

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,91 @@ class Method46App extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Center(
-        child: Image.asset(
-          'images/logo.png',
-          height: 300,
-          width: 300,
-          fit: BoxFit.cover,
+        child: Method46(title: '4-6 Method'),
+        ),
+      );
+  }
+}
+
+class Method46 extends StatefulWidget {
+  Method46({Key key, this.title}) : super(key: key);
+  final String title;
+
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(20),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("4-6 Method"),
+        Container(
+          margin: const EdgeInsets.only(bottom: 8),
+          child: IconButton(
+              icon: Icon(Icons.info_outline, color: Colors.deepOrangeAccent),
+              iconSize: 15,
+              onPressed: (){},
+          ),
+        ),
+      ],
+    ),
+  );
+
+  Widget _buildParamItem(String label, String unit) {
+      return Expanded(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  child: Text(label),
+                ),
+                Text(unit),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    Column _buildParamsGrid() {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildParamItem('Coffee', '[g]'),
+              _buildParamItem('Water', '[g]'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildParamItem('Grinder', '[g]'),
+              _buildParamItem('Grind size', '[g]'),
+            ],
+          ),
+        ],
+      );
+    }
+
+    @override
+    _Method46State createState() => _Method46State();
+}
+
+
+class _Method46State extends State<Method46> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            widget.titleSection,
+            widget._buildParamsGrid(),
+          ],
         ),
       ),
     );
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:method_4_6/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Method46App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
